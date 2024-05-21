cb34c87e6838de93fe16094fda06516ee8a3419b
Ryan S <ryanssteles@gmail.com>
2024-05-21 14:37:48 -0300

    feat: operations implemented

--- internal/app/handler/transform.go
+++ internal/app/handler/transform.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
 func TransformJSON(args []string) error {
@@ -23,9 +26,45 @@ func TransformJSON(args []string) error {
 		return err
 	}
 
+	if len(args) == 0 {
+		log.Println("No operations to perform, copying input.json to output.json")
+		return os.WriteFile("output.json", file, 0644)
+	}
 	// Modify data
 	for _, op := range args {
-		_ = op
+		parts := strings.Split(op, ":")
+		if len(parts) < 2 {
+			return fmt.Errorf("invalid operation: %s", op)
+		}
+		operation, key := parts[0], parts[1]
+
+		switch operation {
+		case "set":
+			if len(parts) != 3 {
+				continue
+			}
+			data[key] = parts[2]
+		case "rename":
+			if len(parts) != 3 {
+				continue
+			}
+			value, ok := data[key];
+			if !ok {
+				log.Printf("key %s not found for operation %s", key, op)
+				continue
+			}
+			newKey := parts[2]
+			delete(data, key)
+			data[newKey] = value
+		
+		case "delete":
+			_, ok := data[key]
+			if !ok {
+				log.Printf("key %s not found for operation %s", key, op)
+				continue
+			}
+			delete(data, key)
+		}
 	}
 
 	modifiedData, err := json.MarshalIndent(data, "", "  ")
